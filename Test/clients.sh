6474200c721dc980565cfe7be7e9d12cbf09445f
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2019-11-25 20:46:15 +0530

    fake

--- Test/clients.sh
+++ Test/clients.sh
@@ -2,7 +2,7 @@
 i=0
 while [ $i -lt $1 ]
 do
-	curl http://127.0.1.1:8080/ &
+	curl http://3.19.188.211/ &
 	true $((i=i+1))
 done
 
--- htdocs/script.js
+++ htdocs/script.js
@@ -1,1 +1,17 @@
 console.log("Developed By Rishabh Anand, BITS GOA");
+function getImage() {
+	var imageHolder = document.getElementById('image-holder');
+	var serverString = "https://swd.bits-goa.ac.in/css/studentImg/";
+	var input = document.getElementById('ID').value;
+	input = input.slice(0,-1);
+	var finalString = serverString + input + ".jpg";
+	var visible = document.getElementById('alerts');
+	if (visible.style.display !== 'block')
+	{
+		console.log('nice');
+		visible.style.display = 'block';
+	}
+	else {
+		visible.style.display = 'none';
+	}
+}
